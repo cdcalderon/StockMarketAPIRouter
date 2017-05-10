122166a805b2d0fd11946a9995ad9230a0fe54ef
carloscalderon <cdcalderon@gmail.com>
2017-05-10 09:28:51 -0500

    Change scheduleJob to use array of symbols

--- server/server.js
+++ server/server.js
@@ -8,20 +8,31 @@ const port = process.env.PORT || 4500;
 const earningUrl = 'http://localhost:4000/threearrowsignals';
 
 schedule.scheduleJob('*/1 * * * *', function() {
-    let symbol = 'aapl';
+    //let symbol = 'aapl';
     let from = '01/01/16';
     let to = '01/01/17';
 
-    axios.get(earningUrl, {
-        params: {
-            symbol: symbol, from: from, to: to
-        }
-    }).then(function(data) {
-        res.send(data.data)
-    }).catch(function(err){
-        res.send(err)
-    });
+    for(let symbol of getStockSymbols()) {
+
+        setTimeout(() => {
+            axios.get(earningUrl, {
+                params: {
+                    symbol: symbol, from: from, to: to
+                }
+            }).then(function(data) {
+                console.log(`Got: ${data}`);
+            }).catch(function(err){
+                console.log(`Error::  ${err}`);
+            });
+        }, 200);
+
+    }
+
 });
+
+let getStockSymbols = () => {
+  return ['aapl', 'msft', 'amzn', 'pnra', 'pcln'];
+};
 
 app.listen(port, () => {
     console.log(`Started up at port ${port}`)
